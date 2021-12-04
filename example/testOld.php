2fed59318e9bf063d4c236e76a679a823600f484
ShockedPlot7560 <66992287+ShockedPlot7560@users.noreply.github.com>
2021-12-04 21:59:54 +0100

    Add multi format support

---
+++ example/testOld.php
@@ -0,0 +1,13 @@
+<?php
+
+use ShockedPlot7560\minecraftskin\Exporter;
+use ShockedPlot7560\minecraftskin\Skin;
+
+require dirname(__DIR__) . "/vendor/autoload.php";
+
+$skin = new Skin(__DIR__ . "/old.png");
+Exporter::exportPng($skin, __DIR__ . "/head.png", Exporter::HEAD_TYPE);
+
+Exporter::exportPng($skin, __DIR__ . "/body.png", Exporter::BODY_TYPE);
+
+Exporter::exportPng($skin, __DIR__ . "/skin.png", Exporter::SKIN_TYPE);
